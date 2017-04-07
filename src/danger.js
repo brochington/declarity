983c664fe27187c5de09dcfd45582fb46fa87de6
Broch Stilley <brochington@gmail.com>
2017-04-06 18:43:59 -0700

    Add danger file

---
+++ src/danger.js
@@ -0,0 +1,9 @@
+const { NODE_ENV } = process.env
+
+export default function danger(condition, errorMessage) {
+    if (NODE_ENV !== 'production') {
+        if (!condition) {
+            throw new Error(errorMessage)
+        }
+    }
+}
